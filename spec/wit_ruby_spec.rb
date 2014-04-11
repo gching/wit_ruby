e2af433c6db914ec78eaccd45bcb379adf3d0d2e
Gavin Ching <gavinchingy@gmail.com>
2014-04-11 10:04:41 -0400

    Add skeleton for the specs off WitRuby

--- spec/wit_ruby_spec.rb
+++ spec/wit_ruby_spec.rb
@@ -1,9 +1,66 @@
 require 'spec_helper'
 
 
-describe WitRuby do 
-  it "does stuff" do
-  	pending # testing that shiz
+describe WitRuby do
+  let (:message) {"Hi there!"}
+
+  it '.send_message(message)' do
+  	pending
   end
-	
+
+  it ".send_sound(soundwave)" do
+    pending
+  end
+
+  it ".message(message_id)" do
+    pending
+  end
+
+  ## Intent_id could be intent id or the name of the intent.
+  it ".intent(intent_id)" do
+    pending
+  end
+
+  ## Returns the list of entities in the current session.
+  it ".entities()" do
+    pending
+  end
+
+  ## Returns the specific entity with the meaning.
+  it ".entities(entity_id)" do
+    pending
+  end
+
+  ## Creates new entity. Requiries JSON.
+  it ".create_entity(new_entity)" do
+    pending
+  end
+
+  it ".update_entitiy(entity_id)" do
+    pending
+  end
+
+  it ".delete_entity(entity_id)" do
+    pending
+  end
+
+  ## Adds a value to the given entitiy. Requires JSON.
+  it ".add_value(entity_id, new_value)" do
+    pending
+  end
+
+  it ".delete.value(entity_id)" do
+    pending
+  end
+
+  ## Adds an expression to the given value in the entity. Requires JSON.
+  it ".add_expression(entity_id, value, new_expression)" do
+    pending
+  end
+
+  ## Deletes the given experession in the value of the entity.
+  it ".delete_expression(entity_id, value, expression)" do
+    pending
+  end
+
 end
