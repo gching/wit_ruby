e79da0a5545eddf4f006c96b4f2e659b78accf9a
Gavin Ching <gavinchingy@gmail.com>
2014-05-05 15:10:59 -0700

    Add webmock and VCR for testing api functionalities

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,12 @@ Coveralls.wear!
 
 require 'wit_ruby'
 require 'pry'
+
+require 'webmock/rspec'
+require 'vcr'
+
+#VCR config
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/fixtures/wit_cassettes'
+  c.hook_into :webmock
+end
