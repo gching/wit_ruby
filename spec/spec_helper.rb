a605f2f3624b5b4f8e1c3579f80ca43864048860
Gavin Ching <gavinchingy@gmail.com>
2014-05-15 16:24:43 -0700

    Add parameter to VCR so that it delays requests in between, passes tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,4 +13,17 @@ require 'timeout'
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/fixtures/wit_cassettes'
   c.hook_into :webmock
+  allow_next_request_at = nil
+  filters = [:real?, lambda { |r| URI(r.uri).host == 'api.wit.ai' }]
+
+  c.after_http_request(*filters) do |request, response|
+    allow_next_request_at = Time.now + 1
+  end
+
+  c.before_http_request(*filters) do |request|
+    if allow_next_request_at && Time.now < allow_next_request_at
+      sleep(allow_next_request_at - Time.now)
+    end
+  end
+
 end
