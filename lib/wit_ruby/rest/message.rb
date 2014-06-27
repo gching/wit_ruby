5adbf3cb347751a38c493de46c46857b76c9cc33
Platform45 <platform45@platfor45simac2.capetown.45cloud.net>
2014-06-27 16:46:15 +0200

    accomodated for changes in wit.ai, responding with more than one outcome, and changed tests accordingly. more work is needed to view more than one outcome.

--- lib/wit_ruby/rest/message.rb
+++ lib/wit_ruby/rest/message.rb
@@ -11,14 +11,14 @@ module Wit
       ##
       ## @return [Float] confidence in the message for the intent.
       def confidence
-        outcome["confidence"]
+        outcomes[0]["confidence"]
       end
 
       ## Returns the intent that this message corresponded to.
       ##
       ## @return [String] intent name that this message corrsponded to.
       def intent
-        outcome["intent"]
+        outcomes[0]["intent"]
       end
 
       ## Generates Array of the names of each entity in this message.
@@ -26,7 +26,7 @@ module Wit
       ## @return [Array] names of each entity
       def entity_names
         entity_arr = Array.new
-        outcome["entities"].each_key do |key|
+        outcomes[0]["entities"].each_key do |key|
           entity_arr << key
         end
         return entity_arr
@@ -40,8 +40,8 @@ module Wit
       ## @param possible_key [Symbol] possible method or key in the hash or entity.
       ## @return [Class] depending on the given results in the data.
       def method_missing(possible_key, *args, &block)
-        if @rawdata["outcome"]["entities"].has_key?(possible_key.to_s)
-          entity_value = @rawdata["outcome"]["entities"][possible_key.to_s]
+        if @rawdata["outcomes"][0]["entities"].has_key?(possible_key.to_s)
+          entity_value = @rawdata["outcomes"][0]["entities"][possible_key.to_s]
           entity_value.class == Hash ? Entity.new(entity_value) : EntityArray.new(entity_value)
         else
           super
